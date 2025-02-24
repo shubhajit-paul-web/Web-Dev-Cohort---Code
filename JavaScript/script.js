e959b9129ceaed21a3f3a591dab47c12c113948d
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-02-24 19:06:28 +0530

    add some more topics

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,28 @@
-let demo = function() {
-    console.log("hello bhai, khelega free fire?");
-}
+// function greetmen(name = "[Please pass the argument]") {
+//     console.log(`Hello, ${name}`);
+// }
 
-demo()
+// greetmen(undefined)
+
+// * JavaScript default parameter mechanism
+
+// Behind the scenes, javascript treats default paramers as:
+
+// function greetmen(name) {
+//     if (name === undefined) {
+//         name = "[Please pass the argument]";
+//     }
+
+//     console.log(`Hello, ${name}`);
+// }
+
+// greetmen(undefined)
+
+
+// fat arrow function
+
+const add = (a, b) => a + b;
+
+let ans = add(5, 10);
+
+console.log(ans);
---
+++ todays-topic.md
@@ -0,0 +1,7 @@
+[1] Functions in JavaScript
+    - Parameters in JavaScript - [required , destructured , rest , default ] ✅
+    - Understanding Immediately Invoked Function Expression(IIFE).
+    - More Functions in JavaScript - [`Arrow Function` , `Fat Arrow` , `Anonymous` , `Higher Order` , `Callback` , `First Class` , `Pure Function` , `Impure Function`]
+    - How Passing Arguments Works: Value vs. Reference
+    - Understanding Scoping in JS - [Global scope , Function scope]
+    - Explore more topics
