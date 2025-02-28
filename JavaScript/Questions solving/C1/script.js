7b309a0bec074bf05250fd56617c2ff9e8fcff93
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-01 01:19:21 +0530

    more

--- JavaScript/Questions solving/C1/script.js
+++ JavaScript/Questions solving/C1/script.js
@@ -100,3 +100,9 @@
 
 // ? Check if "JavaScript" contains "Script" without using .includes().
 
+let sum = 0;
+for(let i = 0; i <= 100; i++) {
+    sum += i;
+}
+
+console.log(sum);
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -29,3 +29,42 @@
 // write variables, functions, etc. out of any scope/boundary
 
 // {}, (), [] - local scope
+
+// Array - Data Structure: []
+
+// const friends = ["Shubhajit", "Debabrata", "Rahul", "Raj", "Raju"];
+
+// const arr = [
+//     ["Alok", "Demitri"],
+//     ["Maxim", "Keli"],
+//     [
+//         "Skyler", 
+//         [
+//             "Glow wall",
+//             "animation"
+//         ]
+//     ],
+// ];
+
+// console.log(arr[2][1][0]);
+
+
+const characters = ["Alok", "Andrew", "Antonio", "Caroline", "Chrono"];
+
+const value = characters.push("D-bee", "Maxim");
+
+// console.log("returned value:", value);
+
+// console.log(characters);
+
+
+let arr = ["B", "D", "A", "C", "F", "E", "G"];
+let nums = [44, 5, 100, 2, 20, 4, 16, 8];
+
+let ans = nums.reverse()
+
+console.log(ans);
+
+
+
+
