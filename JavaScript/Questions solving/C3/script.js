25cd13e4b9f538a21751a577eee0ae57050ab52f
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-14 16:44:26 +0530

    solved one more question

--- JavaScript/Questions solving/C3/script.js
+++ JavaScript/Questions solving/C3/script.js
@@ -1,28 +1,9 @@
-function findDuplicates(arr) {
-    let n = arr.length;
-    
-    if(n === 0 || n === 1) return [];
-    
-    const obj = {};
-    let newArr = [];
-    
-    for(let i = 0; i < n; i++) {
-        if(arr[i] in obj) {
-            obj[arr[i]]++;
-        } else {
-            obj[arr[i]] = 1;
-        }
-    }
-    console.log(obj);
-    
-    // for(let key in obj) {
-    //     if(obj[key] > 1) {
-    //         newArr[newArr.length] = key;
-    //     }
-    // }
-    
-    return newArr;
-}
+// ? Perfect Square
 
-let ans = findDuplicates([2, 4, 5, 5, 3, 0, 1]);
-console.log(ans);
+const num = 25;
+
+if (Number.isInteger(Math.sqrt(num))) {
+    console.log("Perfect Square");
+} else {
+    console.log("It's Perfect Square!");
+}
--- JavaScript/Questions solving/Extra Questions/script.js
+++ JavaScript/Questions solving/Extra Questions/script.js
@@ -569,28 +569,28 @@
 
 // ? Real-world Question 12: Write a program to accept rating of the movie as double and Movie name as String.
 
-function movieRatingAssign(movieName = "", rating = 0) {
-    if (movieName.length > 0 && !isNaN(rating)) {
-        let msg = "";
-        if (rating >= 0 && rating <= 2) {
-            msg = "Flop";
-        } else if (rating >= 2.1 && rating <= 3.4) {
-            msg = "Semi-hit";
-        } else if (rating >= 3.5 && rating <= 4.5) {
-            msg = "Hit";
-        } else if (rating >= 4.6 && rating <= 5) {
-            msg = "Super Hit";
-        }
+// function movieRatingAssign(movieName = "", rating = 0) {
+//     if (movieName.length > 0 && !isNaN(rating)) {
+//         let msg = "";
+//         if (rating >= 0 && rating <= 2) {
+//             msg = "Flop";
+//         } else if (rating >= 2.1 && rating <= 3.4) {
+//             msg = "Semi-hit";
+//         } else if (rating >= 3.5 && rating <= 4.5) {
+//             msg = "Hit";
+//         } else if (rating >= 4.6 && rating <= 5) {
+//             msg = "Super Hit";
+//         }
 
-        return `${movieName} is a ${msg} Movie.`;
-    } else {
-        return "Please pass correct arguments: moveName is in string and rating will be number";
-    }
-}
+//         return `${movieName} is a ${msg} Movie.`;
+//     } else {
+//         return "Please pass correct arguments: moveName is in string and rating will be number";
+//     }
+// }
 
-console.log(movieRatingAssign("KGF", 4.9));
-console.log(movieRatingAssign("RRR", 2.1));
-console.log(movieRatingAssign("Venom", 4.1));
+// console.log(movieRatingAssign("KGF", 4.9));
+// console.log(movieRatingAssign("RRR", 2.1));
+// console.log(movieRatingAssign("Venom", 4.1));
 
 
   
