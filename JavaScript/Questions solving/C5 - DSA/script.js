49da91ff6c32af13f8c76888462e875961f14286
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-17 00:05:09 +0530

    solved 2nd dsa question - a

--- JavaScript/Questions solving/C5 - DSA/script.js
+++ JavaScript/Questions solving/C5 - DSA/script.js
@@ -3,40 +3,62 @@
 number assigned to books. The ISBN is valid if the sum of its digits, each
 multiplied by its position (1 to 10), is divisible by 11. */
 
-const ISBN_Number = prompt("Enter a ISBN number to check it is valid or not.");
+// const ISBN_Number = prompt("Enter a ISBN number to check it is valid or not.");
 
-function isISBNCorrect(ISBN) {
-    if (ISBN) {
-        if (ISBN.toString().length === 10) {
-            let ISBN_Arr = [];
-            for(let i = 0; i < ISBN.length; i++) {
-                if (!isNaN(ISBN[i]) || ISBN[ISBN.length - 1] === "X") {
-                    ISBN_Arr[ISBN_Arr.length] = ISBN[i] !== "X" ? Number(ISBN[i]) : 10;
-                } else {
-                    return `'${ISBN[i]}' is not a number!`;
-                }
-            }
+// function isISBNCorrect(ISBN) {
+//     if (ISBN) {
+//         if (ISBN.toString().length === 10) {
+//             let ISBN_Arr = [];
+//             for(let i = 0; i < ISBN.length; i++) {
+//                 if (!isNaN(ISBN[i]) || ISBN[ISBN.length - 1] === "X") {
+//                     ISBN_Arr[ISBN_Arr.length] = ISBN[i] !== "X" ? Number(ISBN[i]) : 10;
+//                 } else {
+//                     return `'${ISBN[i]}' is not a number!`;
+//                 }
+//             }
     
-            let sumOfDigits = 0;
-            for (let i = 0; i < ISBN_Arr.length; i++) {
-                sumOfDigits += ISBN_Arr[i] * (i + 1);
-            }
+//             let sumOfDigits = 0;
+//             for (let i = 0; i < ISBN_Arr.length; i++) {
+//                 sumOfDigits += ISBN_Arr[i] * (i + 1);
+//             }
     
-            if (sumOfDigits % 11 === 0) {
-                return `🟢 ${ISBN} is a valid ISBN number.`;
-            }
-            return `${ISBN} is a invalid ISBN number!!!`;
-        } else {
-            return "Error: Your ISBN number is incorrect, because it's not equal to 10 digits.";
+//             if (sumOfDigits % 11 === 0) {
+//                 return `🟢 ${ISBN} is a valid ISBN number.`;
+//             }
+//             return `${ISBN} is a invalid ISBN number!!!`;
+//         } else {
+//             return "Error: Your ISBN number is incorrect, because it's not equal to 10 digits.";
+//         }
+//     }
+//     return `Error: Input field is blank, provide a 10 digit number.`;
+// }
+
+// const ans = isISBNCorrect(ISBN_Number); // answer
+
+// if (ans.startsWith("Error")) {
+//     console.error(ans);
+// } else {
+//     console.log(ans);
+// }
+
+
+// ? Question 2: HCF/GCD
+/* Description: The Highest Common Factor (HCF) or Greatest Common Divisor
+(GCD) of two numbers is the largest number that divides both numbers
+without leaving a remainder.
+    Euclidean Algorithm (Efficient Method):
+    Formula: GCD(a, b) = GCD(b, a % b)
+*/
+
+function getGCD(a = 0, b = 0) {
+    if (a || b) {
+        while(b > 0) {
+            [a, b] = [b, a % b];
         }
+        return a;
+    } else {
+        return `Please pass arguments to 'a' and 'b'`;
     }
-    return `Error: Input field is blank, provide a 10 digit number.`;
 }
 
-const ans = isISBNCorrect(ISBN_Number); // answer
-
-if (ans.startsWith("Error")) {
-    console.error(ans);
-} else {
-    console.log(ans);
-}
+console.log(getGCD(12, 18));
