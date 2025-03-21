2bfa9b1f46d6cf6f4ef6d60c989da8f9a77144ee
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-21 20:13:27 +0530

    solve one more pattern

--- JavaScript/Questions solving/C3/script.js
+++ JavaScript/Questions solving/C3/script.js
@@ -104,20 +104,49 @@
 
 
 // ? Number Triangular Pattern
+// const prompt = require("prompt-sync")();
+// const n = Number(prompt("Enter a number: "));
+
+// for(let i = 0; i < n; i++) {
+// 	let nums = "";
+	
+// 	// adding spaces
+// 	for(let j = 0; j < (n-i) - 1; j++) {
+// 		nums += "  ";
+// 	}
+
+// 	// adding nums
+// 	for(let j = 0; j <= i; j++) {
+// 		nums += (i + 1) + "   ";
+// 	}
+// 	console.log(nums);
+// }
+
+
+// ? Hollow Triangle Pattern
 const prompt = require("prompt-sync")();
 const n = Number(prompt("Enter a number: "));
 
 for(let i = 0; i < n; i++) {
-	let nums = "";
-	
-	// adding spaces
-	for(let j = 0; j < (n-i) - 1; j++) {
-		nums += "  ";
+	let stars = "";
+
+	// Adding left side spaces
+	for(let j = 0; j < (n-i); j++) {
+		stars += " ";
 	}
 
-	// adding nums
+	// Adding stars
 	for(let j = 0; j <= i; j++) {
-		nums += (i + 1) + "   ";
+		if (i === (n-1)) {
+			stars += "* ";
+		} else {
+			if (j === 0 || j === i) {
+				stars += "* ";
+			}
+			if (j !== 0 && j !== i) {
+				stars += "  ";
+			}
+		}
 	}
-	console.log(nums);
+	console.log(stars);	
 }
--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const prompt = require("prompt-sync")();
 const n = Number(prompt("Enter a number: "));
 
 for(let i = 0; i < n; i++) {
-	let nums = "";
-	
-	// adding spaces
-	for(let j = 0; j < (n-i) - 1; j++) {
-		nums += "  ";
+	let stars = "";
+
+	// Adding left side spaces
+	for(let j = 0; j < (n-i); j++) {
+		stars += " ";
 	}
 
-	// adding nums
+	// Adding stars
 	for(let j = 0; j <= i; j++) {
-		nums += (i + 1) + "   ";
+		if (i === (n-1)) {
+			stars += "* ";
+		} else {
+			if (j === 0 || j === i) {
+				stars += "* ";
+			}
+			if (j !== 0 && j !== i) {
+				stars += "  ";
+			}
+		}
 	}
-	console.log(nums);
+	console.log(stars);	
 }
