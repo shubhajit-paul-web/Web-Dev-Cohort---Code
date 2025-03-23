0dbdda7f70465fcb37302cd0a5d2d46bdb2e1477
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-24 01:22:32 +0530

    revision

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ let arr = [4, 1, 45, 24, 5, 9, 14, 78];
 let max = -Infinity;
 let sMax = -Infinity;
 
-for (let i = 0; i < arr.length; i++) {
+for(let i = 0; i < arr.length; i++) {
 	if (arr[i] > max) {
 		sMax = max;
 		max = arr[i];
-	} else if (arr[i] > sMax && arr[i] < max) {
+	} else if(arr[i] > sMax && arr[i] < max) {
 		sMax = arr[i];
 	}
 }
 
-console.log(sMax);
-
+console.log(max,sMax);
