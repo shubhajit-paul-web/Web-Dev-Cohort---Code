064b093bb9c4b91472031b3f8b5fc6fe38071736
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-24 01:34:56 +0530

    revision some more questions

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // const prompt = require("prompt-sync")();
 // const n = Number(prompt("Enter a number: "));
+// let arr = [4, 1, 45, 24, 5, 9, 14, 78];
 
-let arr = [4, 1, 45, 24, 5, 9, 14, 78];
+let arr = [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0];
 
-let max = -Infinity;
-let sMax = -Infinity;
+let j = 0;
 
 for(let i = 0; i < arr.length; i++) {
-	if (arr[i] > max) {
-		sMax = max;
-		max = arr[i];
-	} else if(arr[i] > sMax && arr[i] < max) {
-		sMax = arr[i];
+	if (arr[i] === 1) {
+		[arr[j], arr[i]] = [arr[i], arr[j]];
+		j++;
 	}
 }
 
-console.log(max,sMax);
+console.log(arr);
