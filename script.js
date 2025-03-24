a4a99805a987bf09c2518335913646e5f0146a7f
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-03-24 21:12:53 +0530

    revision

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// const prompt = require("prompt-sync")();
-// const n = Number(prompt("Enter a number: "));
-// let arr = [4, 1, 45, 24, 5, 9, 14, 78];
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter a number: "));
 
-let arr = [1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0];
+let countDigits = 0;
 
-let j = 0;
-
-for(let i = 0; i < arr.length; i++) {
-	if (arr[i] === 1) {
-		[arr[j], arr[i]] = [arr[i], arr[j]];
-		j++;
-	}
+while(n > 0) {
+	n = Math.floor(n/10);
+	countDigits++;
 }
 
-console.log(arr);
+console.log(countDigits);
