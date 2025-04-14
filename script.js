d31626dca13ed23c91eefde395e972c02b32f147
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-04-15 01:54:10 +0530

    dom ex - 2

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-const btn = document.querySelector("button");
-const ul = document.querySelector("ul");
-const elemCountText = document.querySelector(".elements-count");
+const imageView = document.querySelector(".image-view img");
+const images = document.querySelectorAll(".img img");
 
-let countElems = 1;
-
-btn.addEventListener("click", function() {
-    if (countElems <= 10) {
-        const li = document.createElement("li");
-        li.textContent = `Item ${countElems}`;
-        ul.appendChild(li);
+images.forEach(function(img) {
+    img.addEventListener("click", function() {
+        imageView.src = img.src;
+        console.log(img.src);
+        
+    });
     
-        elemCountText.textContent = `${countElems}/10 elements created`;
-        countElems++;
-
-        if(countElems === 11) {
-            btn.disabled = true;
-            btn.style.cursor = "not-allowed";
-            btn.style.opacity = "0.8";
-        }
-    }
 });
