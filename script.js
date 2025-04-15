1819c9a0beeb160d1b2122faee2ab3d8b6ad471e
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-04-15 23:07:48 +0530

    practice

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const imageView = document.querySelector(".image-view img");
-const images = document.querySelectorAll(".img img");
-
-images.forEach(function(img) {
-    img.addEventListener("click", function() {
-        imageView.src = img.src;
-        console.log(img.src);
-        
+function characterFrequency(str) {
+    let obj = {}
+    
+    for(let i = 0; i < str.length; i++) {
+        if(str[i].charCodeAt() !== 32) {
+            if (obj.hasOwnProperty(obj[str[i]])) {
+                obj[obj[str[i]]]++
+            } else {
+                obj[obj[str[i]]] = 1;
+            }
+        }
+    }
+    
+    let arr = Object.keys(obj).sort();
+    let obj2 = {}
+    arr.forEach(function(key) {
+        obj2[key] = obj[key] 
     });
     
-});
+    for(let key in obj2) {
+        console.log(key)
+    }
+}
+
+characterFrequency("hello")
