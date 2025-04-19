af8b62b53e5e4d431136634269562a6cf27eb923
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-04-19 20:29:20 +0530

    starting oops

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-// parent elements
-const fruitesList = document.querySelector(".fruites-list");
-const delItemUl = document.querySelector(".del-items");
-const btn = document.querySelector("button");
-
-// How to delete element directly without parent
-// element.remove() or node.remove() -> it will remove the element from document object
-
-btn.addEventListener("click", function() {
-    let childNode = fruitesList.children[0];
-
-    let deletedElement = childNode.parentNode.removeChild(childNode);
-
-    delItemUl.append(deletedElement)
-
-    if (fruitesList.children.length === 0) {
-        btn.disabled = true;
+class Greet {
+    constructor(username,) {
+        this.name = username;
     }
-});
+
+    printMsg() {
+        console.log(`Hello, ${this.name}`);
+    }
+}
+
+const userOne = new Greet("Shubhajit");
+userOne.printMsg();
