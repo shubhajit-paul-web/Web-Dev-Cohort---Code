8f806f203de4fdc7fe970b3a272072d2aac43e9a
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-05-01 01:35:38 +0530

    practice and revision of oops

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let isDataAvailable = true;
-
-const InstaData = new Promise(function(res, rej) {
-    if (isDataAvailable) {
-        res();
-    } else {
-        rej("Instagram data not found!");
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
     }
-});
 
-InstaData.then(function() {
-    console.log("Instagram Data...");
-}).then(function(errorMsg) {
-    console.log(errorMsg);
-})
+    // Private
+    #usernameId = "silent_shubhajit_";
+    #passwordId = 4540789;
+
+    login() {
+        if (this.username === this.#usernameId) {
+            if (this.password == this.#passwordId) {
+                console.log(`Welcome, ${this.#usernameId}`);
+                console.log("Instagram reels...");
+                console.log("Instagram posts...");
+            } else {
+                console.log("Password is incorrect, please try again!");
+            }
+        } else {
+            console.log("Username is incorrect!");
+        }
+    }
+}
+
+const userOne = new User("silent_shubhajit_", 4540789);
+userOne.login();
