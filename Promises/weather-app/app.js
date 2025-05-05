b4c42e78091f876f2ac188589bea87192f9311fb
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-05-05 23:56:58 +0530

    weather-app

---
+++ Promises/weather-app/app.js
@@ -0,0 +1,3 @@
+const API_KEY = "62698d9825315333f8197c915aaa7a2c";
+const searchBar = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-btn");
--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-const pizzaOrder = new Promise((resolve, reject) => {
-    let isOrdered = true;
-
-    if (isOrdered) {
-        resolve("Order successfuly completed...")
-    } else {
-        reject("Sorry, your order is cancled...");
-    }
-});
-
-pizzaOrder
-.then((data) => {
+async function fetchGitHubUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
     console.log(data);
-    return data;
-})
-.then((data) => {
-    console.log(data,"hey... second time");
-})
-.then(() => {
-    console.log("end...");
-    
-})
-.catch((data) => {
-    console.log(data);
-})
+}
+
+fetchGitHubUser("shubhajit-paul-web");
+// console.log("line 8 is executing...");
+// console.log("line 9 is executing...");
+// console.log("end...");
+
